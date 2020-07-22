efb31b6d56ff588f9015377d34c70b81c1bd28c9
graffhero <68558582+graffhero@users.noreply.github.com>
2020-07-22 15:06:09 -0500

    Add files via upload
    day_three_labs

---
+++ day_three.py
@@ -0,0 +1,36 @@
+
+
+a = 10
+b = 3
+
+print(a + b)
+print(a * b)
+print(a - b)
+print(a / b)
+print(a % b)
+
+x = 'python'
+
+print(x[0])
+print(x[-1])
+print(len(x))
+print(x.format().upper())
+
+nums = []
+
+nums.append('10')
+print(nums)
+nums.append('100')
+print(nums)
+nums.append('10')
+print(nums)
+nums.append('500')
+print(nums)
+nums.append('56')
+print(nums)
+nums.pop()
+print(nums)
+nums.sort()
+print(nums)
+
+
